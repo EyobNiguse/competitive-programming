b387f765648333c30aeffa082bf877f57653b616
EyobNiguse <65330385+EyobNiguse@users.noreply.github.com>
2022-04-14 16:46:24 +0300

    Time: 84 ms (5.11%), Space: 18.1 MB (20.29%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int val = 0;
+    sort(nums.begin(),nums.end());
+ 
+        if(nums[0] !=  0 ){
+            return 0;
+        }
+        if(nums.size()  == 1) {
+        
+            return 1;
+        }
+        for(int i=0; i < nums.size()-1;i++){
+          cout <<  nums[i]  <<" ";
+            if( abs(nums[i+1] -  nums[i]) != 1 ){
+          
+              val= nums[i]+1;
+              return val;  
+          }else if( i+1 == nums.size()-1 && nums[i] < nums.size() ){
+            
+              val = nums[i] + 2;
+          }
+ 
+      }
+ 
+        return val;
+    }
+};
