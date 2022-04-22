7d531c449ccd65acc937d287159fb2bc7518db85
EyobNiguse <65330385+EyobNiguse@users.noreply.github.com>
2022-04-22 20:41:26 +0300

    Time: 47 ms (5.09%), Space: 12.8 MB (11.59%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool isOperator(string a){
+    
+        if(a == "+" || a == "-" || a == "/" || a == "*" ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    int getValue(string a , string b, string opr){
+       char operation = opr[0];
+        switch(operation){
+           case '+':
+               return  (stoi(a) + stoi(b));
+               break;
+           case '-':
+               return   (stoi(a) - stoi(b));
+               break;
+           case '*':
+               return  (stoi(a) * stoi(b));
+                break;
+           case '/':
+               return  (stoi(a) / stoi(b));
+            break; 
+       } 
+        return 0;
+    }
+ 
+    int evalRPN(vector<string>& tokens) {
+        stack<string> stck;
+        vector< vector<string>> operations;
+       
+        for(int i = 0; i < tokens.size() ; i++){
+          if(! isOperator(tokens[i])){
+              stck.push(tokens[i]);
+          }else if(isOperator(tokens[i])){
+              
+              string secondOperand = stck.top();
+              stck.pop();
+              string firstOperand = stck.top();
+              stck.pop();
+              int value =  getValue(firstOperand,secondOperand,tokens[i]);
+             cout<< firstOperand <<" "<<  tokens[i] <<" " << secondOperand <<" \n";
+              
+              stck.push(to_string(value));
+              cout<<"top \n"<<stck.top()<<"\n";
+             
+          }
+             // cout<< stck.top() <<" ";
+        }          
+       
+     
+        return stoi(stck.top());
+    }
+                          
+};
