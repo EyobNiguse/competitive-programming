283b7ad7af2565f4ccee8409f38b97d85b8b0105
EyobNiguse <65330385+EyobNiguse@users.noreply.github.com>
2022-11-25 12:18:58 +0300

    Time: 1346 ms (10.74%), Space: 175.3 MB (96.74%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -29,7 +29,7 @@ public:
                    fm.erase(x.second.second);
                    um.erase(x.second.second);
                 }
-                pq.pop();
+               pq.pop();              
             }
         }
         um[key] = value;
