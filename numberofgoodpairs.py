8e5e185f316c2d9d1ba31df486cb2f1084a85316
EyobNiguse <65330385+EyobNiguse@users.noreply.github.com>
2023-01-31 01:11:57 +0300

    Create numberofgoodpairs.py

---
+++ numberofgoodpairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for i in range(len(nums2)):
+            j  =  m -1 + i
+            temp =  nums1[j] 
+            while(nums2[i] < nums1[j] and j >= 0):
+                nums1[j+1] = nums1[j]
+                j-=1
+            nums1[j+1] = nums2[i]
+            print(nums1)
